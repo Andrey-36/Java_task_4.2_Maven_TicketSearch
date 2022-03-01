660f67feae2a0c72fa40c3c30997deed64cd21da
Andrey Kolotukhin <KolotukhinAndrey@rambler.ru>
2022-03-01 15:49:37 +0300

    Added comparator

--- src/main/java/ru/netology/manager/TicketManager.java
+++ src/main/java/ru/netology/manager/TicketManager.java
@@ -3,6 +3,9 @@ package ru.netology.manager;
 import ru.netology.domain.Ticket;
 import ru.netology.repository.TicketRepository;
 
+import java.util.Arrays;
+import java.util.Comparator;
+
 public class TicketManager {
     private TicketRepository repository;
 
@@ -14,10 +17,16 @@ public class TicketManager {
         repository.save(ticket);
     }
 
-    public Ticket[] searchAirport(String DepartureAirport, String ArrivalAirport) {
-        Ticket[] result = new Ticket[0];
+    public class TicketByTravelTimeAscComparator implements Comparator<Ticket> {
+        public int compare(Ticket o1, Ticket o2) {
+            return o1.getTravelTime() - o2.getTravelTime();
+        }
+    }
 
+    public Ticket[] searchAirport(String DepartureAirport, String ArrivalAirport, Comparator<Ticket> comparator) {
+        Ticket[] result = new Ticket[0];
         for (Ticket ticket : repository.findAllAirport("LED", "AER")) {
+
             if (ticket.getDepartureAirport().equalsIgnoreCase(DepartureAirport)) {
                 Ticket[] tmp = new Ticket[result.length + 1];
                 System.arraycopy(result, 0, tmp, 0, result.length);
@@ -31,6 +40,7 @@ public class TicketManager {
                 result = tmp;
             }
         }
+        Arrays.sort(result, comparator);
         return result;
     }
 }
